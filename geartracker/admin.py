108c357d15ea16b2c210b9f8087bf782273c45e0
Thai Nguyen <thai@techfitnesstraining.com>
2019-02-28 15:17:04 -0800

    * updated init files * updated admin UI

--- geartracker/admin.py
+++ geartracker/admin.py
@@ -7,15 +7,6 @@ from django.conf import settings
 from geartracker.models import Bike, Gear, Activity
 
 
-class BikeAdmin(admin.ModelAdmin):
-
-    list_display = ('id','name','brand','model','distance','elevation')
-    list_editable = ('name',)
-    exclude = ('created_by','created_date','modified_by','modified_date')
-    search_fields = ('name','brand','model')
-    save_on_top = True
-
-
 class GearAdmin(admin.ModelAdmin):
 
     list_display = ('id','name','brand','model','distance','elevation','date_installed','date_removed')
@@ -25,6 +16,11 @@ class GearAdmin(admin.ModelAdmin):
     save_on_top = True
 
 
+class GearAdminInline(admin.TabularInline):
+    model = Gear
+    extra = 0
+
+
 class ActivityAdmin(admin.ModelAdmin):
 
     list_display = ('id','title','description','distance','elevation','processed')
@@ -32,6 +28,21 @@ class ActivityAdmin(admin.ModelAdmin):
     exclude = ('created_by','created_date','modified_by','modified_date')
     search_fields = ('title','description')
     save_on_top = True
+
+
+class ActivityAdminInline(admin.TabularInline):
+    model = Activity
+    extra = 0
+
+
+class BikeAdmin(admin.ModelAdmin):
+
+    list_display = ('id','name','brand','model','distance','elevation')
+    list_editable = ('name',)
+    exclude = ('created_by','created_date','modified_by','modified_date')
+    search_fields = ('name','brand','model')
+    save_on_top = True
+    inlines = [GearAdminInline, ActivityAdminInline]
 
 
 admin.site.register(Bike, BikeAdmin)
--- geartracker/models.py
+++ geartracker/models.py
@@ -11,8 +11,8 @@ class Bike(ModifiedDate):
     name = models.CharField(max_length=100, blank=True, null=True)
     brand = models.CharField(max_length=100, blank=True, null=True)
     model = models.CharField(max_length=100, blank=True, null=True)
-    distance = models.IntegerField()
-    elevation = models.IntegerField()
+    distance = models.IntegerField(default=0)
+    elevation = models.IntegerField(default=0)
 
     def __unicode__(self):
         return "{0} ({1})".format(self.name, self.model)
@@ -23,10 +23,10 @@ class Gear(ModifiedDate):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100, blank=True, null=True)
     model = models.CharField(max_length=100, blank=True, null=True)
-    distance = models.IntegerField()
-    elevation = models.IntegerField()
+    distance = models.IntegerField(default=0)
+    elevation = models.IntegerField(default=0)
     date_installed = models.DateTimeField()
-    date_removed = models.DateTimeField()
+    date_removed = models.DateTimeField(blank=True, null=True)
     bike = models.ForeignKey(Bike, related_name='gear', null=True, on_delete=models.SET_NULL)
 
     def __unicode__(self):
@@ -37,9 +37,9 @@ class Activity(ModifiedDate):
 
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=6500, blank=True, null=True)
-    distance = models.IntegerField()
-    elevation = models.IntegerField()
-    processed = models.BooleanField()
+    distance = models.IntegerField(default=0)
+    elevation = models.IntegerField(default=0)
+    processed = models.BooleanField(default=False)
     date_created = models.DateTimeField()
     bike = models.ForeignKey(Bike, related_name='activities', null=True, on_delete=models.SET_NULL)
 

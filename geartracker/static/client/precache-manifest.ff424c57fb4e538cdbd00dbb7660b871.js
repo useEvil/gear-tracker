f0210fb80a616c39f69f9dfb4dd118cf571cdd5a
Thai Nguyen <thai@techfitnesstraining.com>
2019-09-17 15:52:44 -0700

    * added static files

---
+++ geartracker/static/client/precache-manifest.ff424c57fb4e538cdbd00dbb7660b871.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19d0bc31dd71bee52044ca45fbcb996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f4e59ef63d972e91a7b",
+    "url": "/static/js/2.6d0b47d1.chunk.js"
+  },
+  {
+    "revision": "af05d3ab4c2ba0c4383c",
+    "url": "/static/js/main.d490bf10.chunk.js"
+  },
+  {
+    "revision": "d7f1dcb45c22fbf3b58e",
+    "url": "/static/js/runtime~main.88b4a93c.js"
+  }
+]);

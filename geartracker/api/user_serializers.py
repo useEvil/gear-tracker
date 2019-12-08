a7db40ec4bbfd55aa67762ccf96f61dc473a6b23
Thai Nguyen <thai@techfitnesstraining.com>
2019-12-07 17:29:28 -0800

    * fixed register to take first and last name

--- geartracker/api/user_serializers.py
+++ geartracker/api/user_serializers.py
@@ -13,14 +13,16 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             validated_data['username'],
             validated_data['email'],
-            validated_data['password']
+            validated_data['password'],
+            validated_data['first_name'],
+            validated_data['last_name']
         )
         return user
 

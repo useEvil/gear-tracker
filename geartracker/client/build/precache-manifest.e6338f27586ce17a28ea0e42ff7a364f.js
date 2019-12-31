ebae8adcb5ce07f3ae7c18b349e1a84b963094e1
rakan  nesheiwat <ricky.nesheiwat@verys.com>
2019-12-30 18:10:46 -0800

    test build

---
+++ geartracker/client/build/precache-manifest.e6338f27586ce17a28ea0e42ff7a364f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce225bd641a9c52ec25b5e191538faf",
+    "url": "build/index.html"
+  },
+  {
+    "revision": "c181c4a5e8f1417fb8af",
+    "url": "build/static/js/2.83cd1bf2.chunk.js"
+  },
+  {
+    "revision": "d39f415e1fe1ef53aff8",
+    "url": "build/static/js/main.757fa5a8.chunk.js"
+  },
+  {
+    "revision": "a9a9eee224abfdb0e43e",
+    "url": "build/static/js/runtime~main.fe1d45b2.js"
+  },
+  {
+    "revision": "01e0a3ca8c0a03974225c1d973d33926",
+    "url": "build/static/media/selectCaret.01e0a3ca.svg"
+  }
+]);
--- geartracker/client/build/service-worker.js
+++ geartracker/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8456fd8968fac34b22f73c7d4e3e92e7.js"
+  "build/precache-manifest.e6338f27586ce17a28ea0e42ff7a364f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
